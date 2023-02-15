136e430c9d3807d8f84849f3fc05223c030ed270
Bijay Subedi <subedibj3030@gmail.com>
2023-02-15 14:42:38 +0545

    resolve comments

--- day_4/task_1/main.js
+++ day_4/task_1/main.js
@@ -1,14 +1,17 @@
 const buttons = document.querySelectorAll('button')
 const result = document.getElementById('result')
+
 let resultValue = '';
+
 buttons.forEach(button => {
     button.addEventListener('click', e => {
         let buttonValue = e.target.innerText;
-        if (buttonValue == 'C'){
+
+        if (buttonValue === 'C'){
             resultValue = '';
             result.value = resultValue;
         }
-        else if (buttonValue == '='){
+        else if (buttonValue === '='){
             result.value = eval(resultValue);
             resultValue =''
         }
